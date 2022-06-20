04e35fe7ebd1ad6b6b667670d6d0ddfb2c6d9a10
Daniel <danirvicente@gmail.com>
2022-06-21 00:43:56 +0200

    size and order

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,26 +1,28 @@
 const Buttons = () => {
     return (
-        <>
-            <button className="btn">AC</button>
-            <button className="btn">+/-</button>
-            <button className="btn">%</button>
-            <button className="btn">/</button>
-            <button className="btn">7</button>
-            <button className="btn">8</button>
-            <button className="btn">9</button>
-            <button className="btn">X</button>
-            <button className="btn">4</button>
-            <button className="btn">5</button>
-            <button className="btn">6</button>
-            <button className="btn">-</button>
-            <button className="btn">1</button>
-            <button className="btn">2</button>
-            <button className="btn">3</button>
-            <button className="btn">+</button>
-            <button className="btn">0</button>
-            <button className="btn">,</button>
-            <button className="btn">=</button>
-        </>
+        <div className="container">
+            <button className="btn option">AC</button>
+            <button className="btn option">+/-</button>
+            <button className="btn option">%</button>
+            <button className="btn operator">/</button>
+            <button className="btn number">7</button>
+            <button className="btn number">8</button>
+            <button className="btn number">9</button>
+            <button className="btn operator">X</button>
+            <button className="btn number">4</button>
+            <button className="btn number">5</button>
+            <button className="btn number">6</button>
+            <button className="btn operator">-</button>
+            <button className="btn number">1</button>
+            <button className="btn number">2</button>
+            <button className="btn number">3</button>
+            <button className="btn operator">+</button>
+            <button id="btn-0" className="btn number">
+                0
+            </button>
+            <button className="btn number">,</button>
+            <button className="btn operator">=</button>
+        </div>
     );
 };
 
